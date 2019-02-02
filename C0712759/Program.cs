5ee0fb8382c45365d3f3dd54cdfbff09647c4c17
visualstudio <visualstudio@122-PC32>
2019-02-01 19:40:17 -0500

    hh

--- C0712759/Program.cs
+++ C0712759/Program.cs
@@ -10,6 +10,28 @@ namespace C0712759
     {
         static void Main(string[] args)
         {
+            Countryside c = new Countryside();
+            c.run();
         }
     }
+    class Village
+    {
+        public bool isAstrilde;
+        public Village nextVillage;
+        public Village prevVillage;
+        public string villageName;
+    }
+    class Countryside
+    {
+        Village Toronto, Maple, Ajax;
+
+        public void run()
+        {
+            Maple = new Village();
+            Maple.villageName = "Toronto";
+            Maple.nextVillage = Toronto;
+
+        }
+    }
+}
 }
